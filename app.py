57e21d69233ef050df7ff6dfe38e333eff95582a
Selvaram <ganessel@amazon.com>
2026-02-03 14:12:45 -0800

    Project setup

---
+++ app.py
@@ -0,0 +1,7 @@
+# Read all access logs from logs directory and answer the following questions
+
+# 1. Total number of requests
+# 2. Number of errors
+# 3. Top endpoints with errors
+# 4. Number client errors between start and endtime
+# 5. Number server errors between start and endtime
