094734326a39c7893e271b5e2f572438e6e36afc
priyadharshini6508@gmail.com <priyadharshini6508@email.com>
2026-02-04 23:43:49 -0800

    Structured as class methods

--- app.py
+++ app.py
@@ -17,101 +17,88 @@ import heapq
 import glob
 from pathlib import Path
 
-count = 0
-error_count = 0
-client_error = defaultdict(int)
-server_error = defaultdict(int)
-endpoint_error_count = defaultdict(int)
+class LogProcessor:
+
+	def __init__(self) :
+		self.count = 0
+		self.error_count = 0
+		self.client_error = defaultdict(int)
+		self.server_error = defaultdict(int)
+		self.endpoint_error_count = defaultdict(int)
+
+	def read_file(self, file_directory) :
+
+		for f in file_directory.glob('access*.log') :
+
+			with open(f, 'r') as file:
+			    for line in file:
+
+			        self.count += 1 # Total number of requests
+
+			        # 191.162.100.1 [2026-01-21:15:20:13.174982-08:00] "POST /api/order HTTP/1.1" 200
+			        ip, timestamp, method, endpoint, protocol, return_code = line.split(' ')
+
+			        return_code = int(return_code)
+
+			        # Parse Timestamp
+			        date, hour, minute, sec, _ = timestamp.strip('[]').split(':')
+			        hour, minute, sec = int(hour), int(minute), int(sec.split('.')[0])
+			        year, month, day = map(int, date.split('-'))
+
+			        # Check if server/ client error
+			        if 500 <= return_code < 600 :
+			        	self.server_error[(year, month, day, hour, minute, sec)] += 1 
+			        	self.error_count += 1
+			        	self.endpoint_error_count[endpoint] += 1
+
+			        elif 400 <= return_code < 500 :
+			        	self.client_error[(year, month, day, hour, minute, sec)] += 1 
+			        	self.error_count += 1
+			        	self.endpoint_error_count[endpoint] += 1
+
+	def get_top_k_endpoints(self, k) :
+		top_k = []
+		
+		if len(self.endpoint_error_count) < k :
+			print('Not enough endpoints!')
+			return top_k
+		
+		sorted_endpt_error_count = sorted(self.endpoint_error_count.items(), key=lambda x:x[1], reverse=True)
+
+		for i in range(k):
+			top_k.append(sorted_endpt_error_count[i][0])
+
+		return top_k
 
 
-# Open the file and iterate through each line
-# files = []
-# for i in range(6):
-# 	files.append('access'+str(i)+'.log')
+	def get_errors_in_time_range(self, start, end, errors) :
+		error_cnt = 0
+		for key, val in errors.items() :
+			if start <= key <= end :
+				error_cnt += val
 
-def read_file() :
-	global count, error_count
-	file_directory = Path('logs')
+		return error_cnt
 
-	for f in file_directory.glob('access*.log') :
+lp = LogProcessor()
 
-		with open(f, 'r') as file:
-		    for line in file:
+file_directory = Path('logs')
+lp.read_file(file_directory)
 
-		        count += 1 # Total number of requests
+print('Total number of requests: Expected=30 Actual=', lp.count)
 
-		        # 191.162.100.1 [2026-01-21:15:20:13.174982-08:00] "POST /api/order HTTP/1.1" 200
-		        ip, timestamp, method, endpoint, protocol, return_code = tuple(line.split(' ')) 
-		        
-		        # print('ip=', ip) # 191.162.100.1
-		        # print('time=', timestamp) # [2026-01-21:15:20:13.174982-08:00]
-		        # print('method=', method) # "POST
-		        # print('endpoint=', endpoint) # /api/order
-		        # print('protocol=', protocol) # HTTP/1.1"
-		        # print('return_code=', return_code) # 200
+print('Total number of errors: Expected=12 Actual=', lp.error_count)
 
-
-		        return_code = int(return_code)
-
-		        # Parse Timestamp
-		        date, hour, minute, sec, _ = tuple(timestamp.strip('[]').split(':'))
-		        hour, minute, sec = int(hour), int(minute), int(sec.split('.')[0])
-		        year, month, day = map(int, date.split('-'))
-
-		        # print('year=', year) # 2026
-		        # print('month=', month) # 01
-		        # print('day=', day) # 21
-		        # print('hour=', hour) # 15
-		        # print('minute=', minute) # 20
-		        # print('sec=', sec) # 30
-		        
-
-		        # Check if server/ client error
-		        if 500 <= return_code < 600 :
-		        	server_error[(year, month, day, hour, minute, sec)] += 1 
-		        	error_count += 1
-		        	endpoint_error_count[endpoint] += 1
-
-		        elif 400 <= return_code < 500 :
-		        	client_error[(year, month, day, hour, minute, sec)] += 1 
-		        	error_count += 1
-		        	endpoint_error_count[endpoint] += 1
-
-def get_top_k_endpoints(k) :
-	if len(endpoint_error_count) < k :
-		print('Not enough endpoints!')
-		return
-	sorted_endpt_error_count = sorted(endpoint_error_count.items(), key=lambda x:x[1], reverse=True)
-
-	print('Top k endpoints with errors:')
-	for i in range(k):
-		print(sorted_endpt_error_count[i][0])
-
-
-def get_errors_in_time_range(start, end, errors) :
-	error_count = 0
-	for key, val in errors.items() :
-		if start <= key <= end :
-			error_count += val
-
-	return error_count
-
-read_file()
-
-print('Total number of requests:', count)
-
-print('Total number of errors:', error_count)
-
-get_top_k_endpoints(2)
+top_k_endpts = lp.get_top_k_endpoints(k=2)
+print('Top 2 endpoints with errors:', top_k_endpts)
 
 # Number client errors between start and endtime
 # Input can't have leading zeros like 01
 start = (2026, 1, 21, 16, 25, 00)
 end = (2026, 1, 21, 16, 35, 00)
-client_error_count = get_errors_in_time_range(start, end, client_error)
+client_error_count = lp.get_errors_in_time_range(start, end, lp.client_error)
 print('Number client errors between start and endtime:', client_error_count)
 
 
 # Number server errors between start and endtime
-server_error_count = get_errors_in_time_range(start, end, server_error)
+server_error_count = lp.get_errors_in_time_range(start, end, lp.server_error)
 print('Number server errors between start and endtime:', server_error_count)
